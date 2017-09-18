db4f673ceb6e63f0fe793277dc8a0f5a5f254c12
Emily Wells Jimenez and Glen Buck <emily@>
2017-09-18 10:26:18 -0700

    build clickable walrus page with retracted code.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  var elements = ["h1", "p", "img"];
+
+  elements.forEach(function(element) {
+    $(element).click(function() {
+      alert("This is a " + element);
+    });
+  });
+});
